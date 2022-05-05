24f1b662e050616c43bc02113247f7fa91e36a3a
jonfaustothen <jonfaustothen@gmail.com>
2022-05-05 18:43:28 -0400

    updating

--- todoproject/todo/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from todo.models import Task
-
-# Register your models here.
-admin.site.register(Task)
--- todoproject/todo/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from todo.views import TodoDetailView, TodoListView
-
-urlpatterns = [
-    path('', TodoListView.as_view(), name='todo_list'),
-    path('<int:task_id>', TodoDetailView.as_view(), name='task')
-]
